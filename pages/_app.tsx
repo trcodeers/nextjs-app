a42fdb4ff62f827b04fde741bd280aaaca36029f
Tarun kumar singh <tarunks2512@gmail.com>
2022-01-03 22:28:35 +0530

    add material ui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import theme from "../config/mui";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout>
-      <Component {...pageProps} />
-  </Layout>
+  return (
+          <ThemeProvider theme={theme}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+          </ThemeProvider>
+        )
 }
 
 export default MyApp
