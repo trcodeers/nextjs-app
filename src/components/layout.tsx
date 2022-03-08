bab5ecb35201191440cdbfa4334aee6f20a018da
tarun singh <tarunks2512@gmailo.com>
2022-03-08 23:04:00 +0530

    mobile drawer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,9 +13,9 @@ const Layout = (props: any) => {
     const { children, ...restprops } = props
     return (
       <div>
-        <PersistentDrawerLeft sx={{ display: { sm: 'none' } }} {...restprops}>
+        <ResponsiveDrawer sx={{ display: { sm: 'none' } }} {...restprops}>
           {children}
-        </PersistentDrawerLeft>
+        </ResponsiveDrawer>
       </div>
     );
 
--- src/components/mobileDrawer.tsx
+++ src/components/mobileDrawer.tsx
@@ -73,6 +73,7 @@ export default function ResponsiveDrawer(props: Props) {
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
         }}
+        style={{zIndex:1301}}
       >
         <Toolbar>
           <IconButton
