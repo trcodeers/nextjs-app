b1b505075b87aee07fa5af6a597a82d3ab1b3fe1
tarun singh <tarunks2512@gmailo.com>
2022-03-09 10:32:06 +0530

    drawer app bar

---
+++ src/components/Drawer/DrawerAppBar.tsx
@@ -0,0 +1,37 @@
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
+const drawerWidth = 240;
+
+const DrawerAppBar = (props: any) =>{
+
+    const {handleDrawerToggle} = props
+    return(
+        <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+        style={{zIndex:1301}}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+    )
+}
+
+export default DrawerAppBar;
