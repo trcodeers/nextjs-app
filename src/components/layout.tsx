6f0db5d0bc770ae2c90697c35b51ba9ecdac6947
tarun singh <tarunks2512@gmailo.com>
2022-03-09 15:40:06 +0530

    remove unused import

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState } from 'react';
 import DekstopDrawer from './Drawer/DekstopDrawer';
 import MobileDrawer from "./Drawer/MobileDrawer";
 import { isDesktop } from 'react-device-detect';
-import DarkModeContext from '../context/DarkModeContext';
 
 const Layout = (props: any) => {
     
     const [viewPort, setViewPort] = useState<String>()
-    const {  handleDarkModeChange } = useContext(DarkModeContext);
 
     useEffect(() =>{
      setViewPort(isDesktop ? 'dekstop' : 'mobile')
@@ -19,7 +17,6 @@ const Layout = (props: any) => {
       <>
         {viewPort !== 'dekstop' && 
             <MobileDrawer
-              handleDarkModeChange={handleDarkModeChange}
               {...restprops}
             >
           {children}
@@ -28,7 +25,6 @@ const Layout = (props: any) => {
       
         {viewPort === 'dekstop' && 
             <DekstopDrawer
-              handleDarkModeChange={handleDarkModeChange}
               {...restprops}
             >
               {children}
