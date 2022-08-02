1ec414a837b73d3ac9569d02d7890d392488fbf5
tarun singh <tarunks2512@gmailo.com>
2022-08-02 18:16:52 +0530

    refactor

--- src/services/indexDBService.ts
+++ src/services/indexDBService.ts
@@ -1,22 +1,21 @@
 
 
 export const storeObject = (dbName: string, customerData: Array<any>) =>{
- // Open the indexedDB.
-const request = indexedDB.open(dbName, 3);
+    
+    // Open the indexedDB.
+    const request = indexedDB.open(dbName);
 
-request.onupgradeneeded = (event: any) => {
+    request.onupgradeneeded = (event: any) => {
 
-  const db = event.target.result;
+        const db = event.target.result;
 
-  // Create another object store called "names" with the autoIncrement flag set as true.
-  const objStore = db.createObjectStore(dbName, { autoIncrement : true });
+        const objStore = db.createObjectStore(dbName, { autoIncrement : true });
 
-  // Because the "names" object store has the key generator, the key for the name value is generated automatically.
-  customerData.forEach(function(customer) {
-        objStore.add(customer);
-  });
+        customerData.forEach(function(customer) {
+                objStore.add(customer);
+        });
 
-};
+    };
 
 
 }
