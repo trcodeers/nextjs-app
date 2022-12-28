c9fa44df579fa7591c6b43072c2f9f7c5980ffa5
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-12-28 19:09:35 +0530

    drawer

--- src/components/Drawer/DekstopDrawer.tsx
+++ src/components/Drawer/DekstopDrawer.tsx
@@ -67,58 +67,86 @@ export default function DekstopDrawer(props: any) {
     
   const { children } = props
   const [open, setOpen] = React.useState(true);
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   const handleDrawerOpen = () => {
     setOpen(!open);
   };
 
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
     <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar
-            position="fixed"
-            sx={{
-                width: { sm: `calc(100%)` },
-                ml: { sm: `${drawerWidth}px` },
-            }}
-            style={{zIndex: 1301}}
-        >        
-            <Toolbar>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={handleDrawerOpen}
-                    edge="start"
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" noWrap component="div">
-                <Link href={'/'}> 
-                  {GlobalConstants.APP_Name}
-                </Link> 
-                </Typography>
-            </Toolbar>
-        </AppBar>
+    <CssBaseline />
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Responsive drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      aria-label="mailbox folders"
+    >
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
       <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
+        container={container}
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
         }}
-        variant="persistent"
-        anchor="left"
-        open={open}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, },
+        }}
+
       >
-        <DrawerHeader/>
-        <DrawerMenu/>
+        {DrawerMenu}
       </Drawer>
-      <Main open={open}>
-        <DrawerHeader />
-            {children}
-      </Main>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open
+      >
+        {DrawerMenu}
+      </Drawer>
     </Box>
+    <Box
+      component="main"
+      sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+    >
+      <Toolbar />
+     
+      {children}
+
+    </Box>
+  </Box>
   );
 }
